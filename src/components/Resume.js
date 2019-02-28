d89629f1f347d511dc046f2aa7f3d42645fd68e8
Marcel Reppenhagen <marcelreppi@gmail.com>
2019-02-28 23:23:44 +0100

    more info to resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -9,7 +9,13 @@ class Resume extends React.Component {
         <div className="resume-title">Resumé</div>
         <hr></hr>
         <div className="resume-section">About</div>
-        <div className="resume-content">Hi, I'm Marcel</div>
+        <div className="resume-content">
+          Hi, I'm Marcel :)<br/>
+          My hometown is Berlin, where I currently pursue my masters degree
+          in <b>Infomation Systems Management</b> at the <b>Technical University of Berlin</b>.
+          I am a curious student with a passion for programming, web technologies and cloud computing.
+          I love exploring today's technologies and continously learn new things.
+        </div>
         <div className="resume-section">Education</div>
         <div className="resume-content">Learning never stops</div>
         <div className="resume-section">Work experience</div>
