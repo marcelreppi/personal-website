14c5d6dcaa98a2d28554b6ed57c4403f4772f88d
Marcel Reppenhagen <marcelreppi@gmail.com>
2019-03-04 01:12:15 +0100

    minor fix

--- src/pages/404.js
+++ src/pages/404.js
@@ -9,7 +9,9 @@ export default () => {
       <div className="section-container projects-container">
         <p>Whoops!</p>
         <p>
-          I don't know what you did there but I know what you should definitely do:<br/>
+          I don't know what you did there but I know what you should definitely do:
+        </p>
+        <p>
           Subscribe to <a href="https://www.youtube.com/user/PewDiePie">PewDiePie</a>
         </p>
         
