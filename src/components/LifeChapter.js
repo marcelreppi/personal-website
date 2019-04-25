d6d75e30a8792f407138269fbe3c3caf633b72fe
Marcel Reppenhagen <marcelreppi@gmail.com>
2019-04-25 10:45:36 +0200

    fixed mobile resume style bug

--- src/components/LifeChapter.js
+++ src/components/LifeChapter.js
@@ -1,37 +1,37 @@
-import React from 'react'
+import React from "react"
 
-import locationIcon from '../static/img/location.png'
-import durationIcon from '../static/img/duration.png'
+import locationIcon from "../static/img/location.png"
+import durationIcon from "../static/img/duration.png"
 
-import '../styles/LifeChapter.css'
+import "../styles/LifeChapter.css"
 
-export default (props) => {
+export default props => {
   console.log(props)
   return (
     <div className="life-chapter">
-      <div className="chapter-basics">
+      <div className="chapter-overview">
         <span className="info-icon">
-          <img className="info-icon-img" src={props.icon} alt=""></img>
+          <img className="info-icon-img" src={props.icon} alt="" />
         </span>
         <b className="info">{props.title}</b>
         <span className="info-icon">
-          <img className="info-icon-img" src={locationIcon} alt=""></img>
+          <img className="info-icon-img" src={locationIcon} alt="" />
         </span>
         <div className="info">{props.location}</div>
         <span className="info-icon">
-          <img className="info-icon-img" src={durationIcon} alt=""></img>
+          <img className="info-icon-img" src={durationIcon} alt="" />
         </span>
         <div className="info">{props.duration}</div>
       </div>
-      {
-        props.details ? 
+      <div className="chapter-details">
+        {props.details ? (
           <ul>
-            { props.details.map( d => {
-              return <li>{d}</li>
-            }) }
+            {props.details.map((d, i) => {
+              return <li key={i}>{d}</li>
+            })}
           </ul>
-        : null
-      }
+        ) : null}
+      </div>
     </div>
   )
 }
