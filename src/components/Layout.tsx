ed5d9a587dc38b2e0803c153de5ee69819f9c717
Marcel Reppenhagen <marcelreppi@gmail.com>
2021-03-12 23:52:47 +0100

    fix navbar fucking finally

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,33 +1,15 @@
 import React, { useState } from "react"
-import { useEffect } from "react"
 import { NavBar } from "./NavBar"
 
 interface LayoutProps {}
 
 export const Layout: React.FC<LayoutProps> = ({ children }) => {
-  const isMobile = () => {
-    if (typeof window === "undefined") {
-      // Assume mobile by default
-      return true
-    }
-
-    return window.innerWidth <= 480
-  }
-  const [showMobileView, setShowMobileView] = useState(isMobile())
-
-  useEffect(() => {
-    window.addEventListener("resize", () => {
-      setShowMobileView(isMobile())
-    })
-  })
-
   const [mobileMenuIsOpen, setMobileMenuIsOpen] = useState(false)
   const toggleMobileMenu = () => setMobileMenuIsOpen(!mobileMenuIsOpen)
 
   return (
     <div className={`${mobileMenuIsOpen ? "fixed" : ""}`}>
       <NavBar
-        mobile={showMobileView}
         mobileMenuIsOpen={mobileMenuIsOpen}
         toggleMobileMenu={toggleMobileMenu}
       ></NavBar>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -18,12 +18,10 @@ const NavBarItem: React.FC<NavBarItemProps> = ({ active, children }) => {
 }
 
 interface NavBarProps {
-  mobile: boolean
   mobileMenuIsOpen: boolean
   toggleMobileMenu: () => void
 }
 export const NavBar: React.FC<NavBarProps> = ({
-  mobile,
   mobileMenuIsOpen,
   toggleMobileMenu,
 }) => {
@@ -59,48 +57,53 @@ export const NavBar: React.FC<NavBarProps> = ({
     )
   })
 
-  const mobileNavBar = (
-    <div
-      className={`w-full h-full bg-white ${
-        mobileMenuIsOpen ? "absolute z-10" : ""
-      }`}
-    >
-      <div className="flex justify-end m-5">
-        <StaticImage
-          layout="fixed"
-          className={`${mobileMenuIsOpen ? "" : "hidden"}`}
-          placeholder="blurred"
-          width={48}
-          height={48}
-          alt="Open menu"
-          src="../images/menu-open.png"
-          onClick={toggleMobileMenu}
-          loading="eager"
-          quality={100}
-        />
-        <StaticImage
-          layout="fixed"
-          className={`${mobileMenuIsOpen ? "hidden" : ""}`}
-          placeholder="blurred"
-          width={48}
-          height={48}
-          alt="Closed menu"
-          src="../images/menu-closed.png"
-          onClick={toggleMobileMenu}
-          loading="eager"
-          quality={100}
-        />
-      </div>
-      <nav className="flex flex-col items-center text-2xl space-y-5">
-        {mobileMenuIsOpen ? navBarItems : null}
+  return (
+    <>
+      {/* Regular NavBar */}
+      <nav className="hidden sm:flex container max-w-5xl mx-auto mt-24 mb-10 justify-around items-center text-2xl">
+        {navBarItems}
       </nav>
-    </div>
-  )
 
-  const regularNavBar = (
-    <nav className="container max-w-5xl mx-auto mt-24 mb-10 flex justify-around items-center text-2xl">
-      {navBarItems}
-    </nav>
+      {/* Mobile NavBar */}
+      <div
+        className={`block sm:hidden w-full h-full bg-white ${
+          mobileMenuIsOpen ? "absolute z-10" : ""
+        }`}
+      >
+        <div className="flex justify-end m-5">
+          <StaticImage
+            layout="fixed"
+            className={`${mobileMenuIsOpen ? "" : "hidden"}`}
+            placeholder="blurred"
+            width={48}
+            height={48}
+            alt="Open menu"
+            src="../images/menu-open.png"
+            onClick={toggleMobileMenu}
+            loading="eager"
+            quality={100}
+          />
+          <StaticImage
+            layout="fixed"
+            className={`${mobileMenuIsOpen ? "hidden" : ""}`}
+            placeholder="blurred"
+            width={48}
+            height={48}
+            alt="Closed menu"
+            src="../images/menu-closed.png"
+            onClick={toggleMobileMenu}
+            loading="eager"
+            quality={100}
+          />
+        </div>
+        <nav
+          className={`${
+            mobileMenuIsOpen ? "flex" : "hidden"
+          } flex-col items-center text-2xl space-y-5`}
+        >
+          {navBarItems}
+        </nav>
+      </div>
+    </>
   )
-  return mobile ? mobileNavBar : regularNavBar
 }
