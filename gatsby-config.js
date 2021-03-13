5264d1d9c693d10ca313ee40b25408ca8330b35b
Marcel Reppenhagen <marcelreppi@gmail.com>
2021-03-13 14:15:34 +0100

    add ga

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+require("dotenv").config()
+
 module.exports = {
   siteMetadata: {
     title: "gatsby",
@@ -5,9 +7,17 @@ module.exports = {
   plugins: [
     "gatsby-plugin-image",
     {
-      resolve: "gatsby-plugin-google-analytics",
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingId: "G-ZB8N8617BP",
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          process.env.GA_MEASUREMENT_ID, // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          anonymize_ip: true,
+        },
       },
     },
     "gatsby-plugin-react-helmet",
