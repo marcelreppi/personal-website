dc08584c807aaaa22028a4f34529cb50fd068fe5
Marcel Reppenhagen <marcelreppi@gmail.com>
2021-10-03 16:24:06 +0200

    format. fix favicon.

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -11,14 +11,10 @@ export const SEO: React.FC<SEOProps> = ({ title, description, slug }) => {
   const url = `https://reppenhagen.space${slug}`
   const twitterUsername = "marcelreppi"
   return (
-    <Helmet
-      title={title}
-      titleTemplate="%s | Marcel Reppenhagen"
-      defaultTitle="Marcel Reppenhagen"
-    >
+    <Helmet title={title} titleTemplate="%s | Marcel Reppenhagen" defaultTitle="Marcel Reppenhagen">
       <html lang="en" />
       <meta charSet="utf-8" />
-      <link rel="shortcut icon" href="favicon.png" type="image/x-icon" />
+
       <link rel="canonical" href={url} />
       {description && <meta name="description" content={description} />}
 
@@ -29,9 +25,7 @@ export const SEO: React.FC<SEOProps> = ({ title, description, slug }) => {
 
       {/* Twitter */}
       <meta name="twitter:card" content="summary_large_image" />
-      {twitterUsername && (
-        <meta name="twitter:creator" content={twitterUsername} />
-      )}
+      {twitterUsername && <meta name="twitter:creator" content={twitterUsername} />}
       {title && <meta name="twitter:title" content={title} />}
       {description && <meta name="twitter:description" content={description} />}
     </Helmet>
