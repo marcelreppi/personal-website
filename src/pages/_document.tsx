1c935002ab1568bcf489c6bf624c6c457e0c333e
Marcel <marcelreppi@gmail.com>
2022-10-08 20:22:23 +0300

    Test without data-domains

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -15,7 +15,7 @@ class MyDocument extends Document {
             defer
             data-website-id="b300b400-004a-409e-97b3-e48b5f6eee7e"
             src="https://umami.marcelreppi.com/umami.js"
-            data-domains="marcelreppi.com"
+            // data-domains="marcelreppi.com"
           ></script>
         </Head>
         <body>
