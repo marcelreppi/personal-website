395a472daacf779e73c779d2954b3be3afe13d25
Marcel <marcelreppi@gmail.com>
2022-10-08 20:11:54 +0300

    Remove umami option

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -15,7 +15,6 @@ class MyDocument extends Document {
             defer
             data-website-id="b300b400-004a-409e-97b3-e48b5f6eee7e"
             src="https://umami.marcelreppi.com/umami.js"
-            data-do-not-track="true"
             data-domains="marcelreppi.com"
           ></script>
         </Head>
